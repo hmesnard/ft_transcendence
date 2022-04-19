c35d3290f34a458208de9b8f511c0b6e5997dfee
bocal <bocal@bocal-6.s19.be>
2022-04-19 16:37:17 +0200

    change app.modul

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -7,26 +7,41 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ConfigModule } from '@nestjs/config';
 import { ChannelModule } from './channel/channel.module';
 import { ChannelController } from './channel/channel.controller';
-import { ChannelModule } from './channel/channel.module';
+// import { ChannelModule } from './channel/channel.module';
+
 
 @Module({
-  imports: [
-    ConfigModule.forRoot({ isGlobal: true }),
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: 'localhost',
-      port: 5432,
-      username: process.env.POSTGRES_USER,
-      password: process.env.POSTGRES_PASSWORD,
-      database: process.env.POSTGRES_DB,
-      entities: ['dist/**/*.entity{.ts,.js}'],
-      synchronize: true
-    }),
-    UserModule,
-    AuthModule,
-    ChannelModule
-  ],
-  controllers: [AppController, ChannelController],
+  imports: [TypeOrmModule.forRoot({
+    type : 'postgres',
+    url: process.env.DATABASE_URL,
+    entities: ["dist/**/*.entity{.ts,.js}"],
+    autoLoadEntities: true,
+    synchronize: true,
+    
+  }), UserModule, ChannelModule],
+  controllers: [AppController,ChannelController],
   providers: [AppService],
 })
+
+
+// @Module({
+//   imports: [
+//     ConfigModule.forRoot({ isGlobal: true }),
+//     TypeOrmModule.forRoot({
+//       type: 'postgres',
+//       host: 'localhost',
+//       port: 5432,
+//       username: process.env.POSTGRES_USER,
+//       password: process.env.POSTGRES_PASSWORD,
+//       database: process.env.POSTGRES_DB,
+//       entities: ['dist/**/*.entity{.ts,.js}'],
+//       synchronize: true
+//     }),
+//     UserModule,
+//     AuthModule,
+//     ChannelModule
+//   ],
+//   controllers: [AppController, ChannelController],
+//   providers: [AppService],
+// })
 export class AppModule {}
