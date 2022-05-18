514f056efb82acdebf142fa98792eedbe5d10a98
nubanum <mikko_n@hotmail.com>
2022-05-18 13:03:16 +0200

    kk

--- backend/src/user/dto/new-user.dto.ts
+++ backend/src/user/dto/new-user.dto.ts
@@ -1,10 +1,12 @@
-import { IsNotEmpty } from "class-validator";
+import { IsNotEmpty, IsNumber, IsString } from "class-validator";
 
 export class NewUserDto {
 
     @IsNotEmpty()
+    @IsNumber()
     id: number;
 
     @IsNotEmpty()
+    @IsString()
     username: string;
 }
