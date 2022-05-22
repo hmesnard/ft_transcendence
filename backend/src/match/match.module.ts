c1557268e61f85af73c4ce589731d560d4f81dc6
nubanum <mikko_n@hotmail.com>
2022-05-22 20:39:03 +0200

    kk

--- backend/src/match/match.module.ts
+++ backend/src/match/match.module.ts
@@ -2,6 +2,7 @@ import { Module } from '@nestjs/common';
 import { JwtModule } from '@nestjs/jwt';
 import { PassportModule } from '@nestjs/passport';
 import { TypeOrmModule } from '@nestjs/typeorm';
+import { UserEntity } from 'src/user/entities/user.entity';
 import { UserModule } from 'src/user/user.module';
 import { MatchEntity } from './entities/match.entity';
 import { MatchController } from './match.controller';
@@ -9,7 +10,7 @@ import { MatchService } from './match.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([MatchEntity]),
+    TypeOrmModule.forFeature([MatchEntity, UserEntity]),
     UserModule, PassportModule.register({ defaultStrategy: 'jwt' }),
     JwtModule.register({
       secret: process.env.JWT_SECRET,
--- backend/src/match/match.service.ts
+++ backend/src/match/match.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { UserEntity, UserLevel } from 'src/user/entities/user.entity';
 import { UserService } from 'src/user/user.service';
 import { Repository } from 'typeorm';
 import { MatchDto } from './dto/match.dto';
@@ -8,10 +9,48 @@ import { MatchEntity } from './entities/match.entity';
 @Injectable()
 export class MatchService {
     constructor(
-        @InjectRepository(MatchEntity)
-        private matchRepository: Repository<MatchEntity>,
+        @InjectRepository(MatchEntity) private matchRepository: Repository<MatchEntity>,
+        @InjectRepository(UserEntity) private userRepository: Repository<UserEntity>,
         private userService: UserService
     ) {}
+
+    async addUserRank(user: UserEntity)
+    {
+        const map1 = new Map();
+        const allPlayers = await this.userRepository.find();
+        for (const player of allPlayers)
+            map1.set(player.id, player.wins - player.losses);
+        const mapSort1 = new Map([...map1.entries()].sort((a, b) => b[1] - a[1]));
+        const index = mapSort1.get(user.id);
+        user.rank = index;
+        await this.userRepository.save(user);
+    }
+
+    async addNewLevel(user: UserEntity)
+    {
+        if (user.wins - user.losses > 5)
+            user.level = UserLevel.advanced;
+        if (user.wins - user.losses > 10)
+            user.level = UserLevel.advanced;
+        if (user.wins - user.losses > 15)
+            user.level = UserLevel.advanced;
+        await this.userRepository.save(user);   
+    }
+
+    async addNewStats(homePlayer: UserEntity, awayPlayer: UserEntity, winner: UserEntity)
+    {
+        if (winner.id === homePlayer.id)
+        {
+            homePlayer.wins++;
+            awayPlayer.losses++;
+        }
+        else
+        {   awayPlayer.wins++;
+            homePlayer.losses++;
+        }
+        await this.userRepository.save(homePlayer);
+        await this.userRepository.save(awayPlayer);
+    }
 
     async saveMatch(matchData: MatchDto) {
         const match = this.matchRepository.create();
@@ -21,6 +60,12 @@ export class MatchService {
         match.winner = await this.userService.getUserById(matchData.winnerId);
         match.homeScore = matchData.homeScore;
         match.awayScore = matchData.awayScore;
+
+        await this.addNewStats(match.homePlayer, match.awayPlayer, match.winner);
+        await this.addNewLevel(match.homePlayer);
+        await this.addNewLevel(match.awayPlayer);
+        await this.addUserRank(match.homePlayer);
+        await this.addUserRank(match.awayPlayer);
 
         return this.matchRepository.save(match);
     }
